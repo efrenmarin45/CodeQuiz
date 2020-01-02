824bb71533e1b218c0dd6428ca7d6a68d5b03a3e
Efren_Marin <efren45marin@gmail.com>
2020-01-01 19:16:00 -0800

    Added additonal styling and finished the questions for the quiz.

--- JS Files/questions.js
+++ JS Files/questions.js
@@ -8,6 +8,55 @@ var questions = [
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
+  },
+  {
+    title: "If I wanted to make a list of items and have them numbered, I would use:",
+    choices: ["ul", "dl", "li", "ol"],
+    answer: "ol"
+  },
+  {
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["javascript", "js", "scripted", "script"],
+    answer: "script"
+  },
+  {
+    title: "Which of the following is the correct syntax to display “Wingardium Leviosa” in an alert box using JavaScript?",
+    choices: ["alertbox(“Wingardium Leviosa”)", "msg(“Wingardium Leviosa”)", "alert(“Wingardium Leviosa”)", "msgbox(“Wingardium Leviosa”)",],
+    answer: "alert(“Wingardium Leviosa”)"
+  },
+  {
+    title: "What is the correct syntax for referring to an external script called “reaper.js”?",
+    choices: ["<script name=”reaper.js”>", "<script href=”reaper.js”>", "<script src=”reaper.js”>", "<script ref=”reaper.js”>"],
+    answer: "<script src=”reaper.js”>"
+  },
+  {
+    title: "What is the syntax for creating a function in JavaScript named as overwatch?",
+    choices: ["function = overwatch()", "function overwatch()", "function : overwatch()", "function := overwatch()"],
+    answer: "function overwatch()"
+  },
+  {
+    title: "How is the function called in JavaScript?",
+    choices: ["call overwatch()", "call function overwatch()", "function overwatch()", "overwatch()"],
+    answer: "overwatch()"
+  },
+  {
+    title: "How to write an ‘if’ statement for executing some code. If “i” is NOT equal to 5?",
+    choices: ["if(i!=5)", "if i<>5", "if(i<>5)", "if i!=5"],
+    answer: "if(i!=5)"
+  },
+  {
+    title: "If we want define style for an unique element, then which css selector will we use ?",
+    choices: ["button", "class", "id", "form"],
+    answer: "id"
+  },
+  {
+    title: "Suppose we want to arrange five different DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
+    choices: ["x-index", "y-index", "z-index", "index-placement"],
+    answer: "z-index"
+  },
+  {
+    title: "If we want to use a nice looking green dotted border around an image, which css property will we use?",
+    choices: ["border-color", "border-style", "border-decoration", "border-line"],
+    answer: "border-style"
   }
-  ///etc.
 ];

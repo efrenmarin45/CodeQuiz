cbe64eba7a9a61ca67757c43a1f381242813f1d5
Efren_Marin <efren45marin@gmail.com>
2020-01-01 21:56:11 -0800

    Fixed small issues with timing and added styling to the questions and start screen.

--- JS Files/script.js
+++ JS Files/script.js
@@ -1,21 +1,27 @@
 const timerElement = document.querySelector(".timer");
 const startQuizElement = document.querySelector("#start-quiz");
 
-var secondsLeft = 60;
+var secondsLeft = 90;
 var score = 0;
 var currentQuestionIndex = 0;
+var time = questions.length * 13;
 
+
+//Sets up timer
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timerElement.textContent = secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
       endQuiz();
     }
   }, 1000);
 }
 
+
+//Sets up beginning of quiz
 startQuizElement.addEventListener("click", function() {
   var startScreenEl = document.getElementsByClassName("start-screen");
   startScreenEl[0].setAttribute("class", "hide");
@@ -23,14 +29,21 @@ startQuizElement.addEventListener("click", function() {
   var questionsEl = document.getElementById("questions");
   questionsEl.removeAttribute("class");
 
+  var timerEl = document.getElementById("timerTitle");
+  timerEl.removeAttribute("class");
+
   setTime();
   getQuestion();
+
 });
+
+
+//Cycles through the questions
 function getQuestion() {
-    console.log("questions:" +JSON.stringify(questions))
-    console.log("index: " +currentQuestionIndex)
+    console.log("questions:" + JSON.stringify(questions))
+    console.log("index: " + currentQuestionIndex)
   var currentQuestion = questions[currentQuestionIndex];
-    console.log("currentQuestions: " +currentQuestion)
+    console.log("currentQuestions: " + currentQuestion)
   var titleEl = document.getElementById("question-title");
   titleEl.textContent = currentQuestion.title;
 
@@ -41,27 +54,29 @@ function getQuestion() {
     var choiceNode = document.createElement("button");
     choiceNode.setAttribute("class", "choice");
     choiceNode.setAttribute("value", choice);
-    choiceNode.textContent = i + 1 + ". " + choice; //1. strings
+    choiceNode.textContent = choice;
     choiceNode.onclick = choiceClick;
     choicesEl.appendChild(choiceNode);
-    //<button class="choice" value="strings">Strings</button>
   });
 }
+
+
+//Penalizes user for wrong answer
 function choiceClick(){
     console.log("clicked on choice")
     if(this.value !== questions[currentQuestionIndex].answer){
-      time -=15;
-      //display wrong
-      if(time < o){
-        time=0;
+      secondsLeft -=10;
+      var wrongEl = document.getElementById("answer-result")
+      wrongEl.innerText("Sorry! That's the wrong answer! Penalty: -10 seconds.")
+      if(secondsLeft <= 0){
+        secondsLeft = 0
       }
-      //update timer on the scree;
     }
     else{
-    //display user saying correct
+    //display to user saying answer is correct
     }
     currentQuestionIndex++;
-    if(currentQuestionIndex === questions.lenght){
+    if(currentQuestionIndex === questions.length){
       endQuiz();
     }
     else{
@@ -69,11 +84,12 @@ function choiceClick(){
     }
 }
 
+
+//Ends the Quiz
 function endQuiz() {
-  //TODO Reference the 'Game Over' screen when timer runs out
   clearInterval(timerInterval);
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
+  var gameover = document.getElementById("end-screen");
+  gameover.removeAttribute("class");
 
   var finalScoreEl = document.getElementById("final-score");
   finalScoreEl.textContent = score;
